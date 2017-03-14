335f97a194ac1a22e17c763d5a5b8aaf224e199c
Edward Hughes <e.f.hughes@qmul.ac.uk>
2017-03-14 22:15:14 +0000

    before refactor

--- learn.py
+++ learn.py
@@ -17,18 +17,19 @@ np.random.seed(0)
 # Parameters
 learning_rate = 0.0001
 num_episodes = 501
-initial_eps = 1
-discount = 0.9
-batch_size = 10
-buffer = 40
+initial_eps = 0.5
+eps_decay = 0.999
+discount = 0.7
+batch_size = 40
+buffer = 3000
 log_freq = 10
 num_runs = 1
-reg_constant = 0
+reg_constant = 0.01
 grad_update_freq = 50
 
 # Network Parameters
-n_hidden_1 = 20 # 1st layer number of nodes
-n_hidden_2 = 20 # 2nd layer number of nodes
+n_hidden_1 = 30 # 1st layer number of nodes
+n_hidden_2 = 30 # 2nd layer number of nodes
 n_input = 4 # input layer (state)
 n_output = 2 # output layer (q values for actions)
 
@@ -45,14 +46,14 @@ target_output = tf.placeholder(shape=[1,n_output],dtype=tf.float32)
 tvars = tf.trainable_variables()
 reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
 loss = tf.reduce_sum(tf.square(target_output - current_output)) + reg_constant * sum(reg_losses)
-trainer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+trainer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 # frozen network for stability
 W1Grad = tf.placeholder(tf.float32,name="batch_grad1")
 W2Grad = tf.placeholder(tf.float32,name="batch_grad2")
 WoutGrad = tf.placeholder(tf.float32,name="batch_grad3")
 batchGrad = [W1Grad,W2Grad,WoutGrad]
 # ignore hidden weights which it somehow knows we defined
-newGrads = tf.gradients(loss, tvars)[0:2]
+newGrads = tf.gradients(loss, tvars)[0:3]
 updateGrads = trainer.apply_gradients(zip(batchGrad, tvars))
 # updateModel = trainer.minimize(loss)
 
@@ -91,7 +92,7 @@ for alpha in range(num_runs):
             d = False
             j = 0
             # The q-network
-            while j < 10000:
+            while j < 1000:
                 # env.render()
                 j+=1
                 # Choose an action greedily (with e chance of random action) from the Q-network
@@ -102,45 +103,44 @@ for alpha in range(num_runs):
                     a = env.action_space.sample()
                 # Get new state and reward from environment
                 s1,r,d,_ = env.step(a)
-                if d == True:
-                    # Reduce chance of random action as we train the model
-                    r = -50
-                    eps -= initial_eps / (num_episodes - 50)
-                    eps = max(eps, 0)
-                    replay.append((s, a, r, s1))
-                    priority.append(0.001)
-                    stepUntilDoneList.append(j)
-                    break
+                if d:
+                    # Penalise failure (should we do this?)
+                    if r < 200:
+                        r -= 50
                 if len(replay) < buffer:  # if buffer not filled, add to it
                     replay.append((s, a, r, s1))
                     priority.append(0.001)
-                    rTotal += r
-                    s = copy.copy(s1)
-                    continue
+                    if d:
+                        rTotal += r
+                        break
                 # buffer is full - overwrite values
-                if (h < (buffer - 1)):
-                    h += 1
                 else:
-                    h = 0
-                replay[h] = (s, a, r, s1)
-                priority[h] = np.max(priority)
+                    if (h < (buffer - 1)):
+                        h += 1
+                    else:
+                        h = 0
+                    replay[h] = (s, a, r, s1)
+                    priority[h] = 0.001 # np.max(priority)
                 s = copy.copy(s1)
                 rTotal += r
                 # randomly sample our experience replay memory
                 # minibatch_indices = np.random.choice(range(len(replay)), batch_size,
                                                      # p=[np.sqrt(a)/sum([np.sqrt(b) for b in priority]) for a in priority])
+                if not (batch_size < len(replay)):
+                    break
                 minibatch_indices = np.random.choice(range(len(replay)), batch_size)
                 for index in minibatch_indices:
+                    temp_loss = 0
                     memory = replay[index]
                     s_mem, a_mem, r_mem, s1_mem = memory
-                    q = sess.run(current_output, feed_dict={inputs[0]: [s_mem]})[0]
+                    target_q = sess.run(current_output, feed_dict={inputs[0]: [s_mem]})[0]
                     # Obtain the Q' values by feeding the new state through our network
                     q_prime = sess.run(current_output, feed_dict={inputs[0]: [s1_mem]})[0]
                     # Obtain max Q' and set our target value for chosen action
                     max_q_prime = np.max(q_prime)
-                    target_q = q
                     # Q-learning
-                    target_q[a_mem] = np.clip(r_mem + discount * max_q_prime,-1,1)
+                    target_q[a_mem] = np.clip(r_mem + discount * max_q_prime,-200,300)
+                    # print(target_q[a_mem])
                     # print(target_q)
                     # SARSA
                     # target_q[a_mem] = np.clip(r_mem + discount * q_prime[a_mem],-1,1)
@@ -148,8 +148,8 @@ for alpha in range(num_runs):
                     # why do we have to do these sequentially?
                     tGrad = sess.run(newGrads, feed_dict={inputs[0]: [s_mem], target_output: [target_q]})
                     loss_val = sess.run(loss, feed_dict={inputs[0]: [s_mem], target_output: [target_q]})
-                    priority[index] = abs(loss_val)
-                    lossTotal += loss_val
+                    # priority[index] = abs(loss_val)
+                    temp_loss += loss_val
                     for ix, grad in enumerate(tGrad):
                         # accumulate the gradients
                         gradBuffer[ix] += grad
@@ -158,9 +158,20 @@ for alpha in range(num_runs):
                         sess.run(updateGrads, feed_dict={W1Grad: gradBuffer[0], W2Grad: gradBuffer[1], WoutGrad: gradBuffer[2]})
                         for ix, grad in enumerate(gradBuffer):
                             gradBuffer[ix] = grad * 0
-                lossTotal /= batch_size
-            sList.append(s)
-            jList.append(j)
+                lossTotal += temp_loss / batch_size
+                if d:
+                    # Reduce chance of random action as we train the model
+                    # eps -= initial_eps / (num_episodes)
+                    eps = eps * eps_decay
+                    eps = max(eps, 1e-5)
+                    # get loss per iteration, a better measure
+                    lossTotal = lossTotal / j
+                    # replay.append((s, a, r, s1))
+                    # priority.append(0.001)
+                    stepUntilDoneList.append(j)
+                    break
+            # sList.append(s)
+            # jList.append(j)
             rList.append(rTotal)
             lossList.append(lossTotal)
             if i % log_freq == 0:
