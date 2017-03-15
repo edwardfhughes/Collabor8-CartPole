98558ead4fb8ee3233490968eee0efdc258e15fd
Edward Hughes <e.f.hughes@qmul.ac.uk>
2017-03-15 14:07:26 +0000

    CartPole solved, refactor needed

--- learn.py
+++ learn.py
@@ -1,210 +1,154 @@
 import numpy as np
 import tensorflow as tf
-import env as environment
 import perceptron
-import lstm_network
 import matplotlib.pyplot as plt
-import random
-import copy
 import gym
 
-# env = environment.ShoprEnv()
+# Import CartPole environment
 env = gym.make('CartPole-v0')
-reset = tf.reset_default_graph()
 
-np.random.seed(0)
+# Start with a clean slate
+tf.reset_default_graph()
 
 # Parameters
-learning_rate = 0.0001
-num_episodes = 501
+# Needs to be large enough to make progress
+learning_rate = 0.01
+num_episodes = 1001
+# Encourage exploring initially
 initial_eps = 0.5
-eps_decay = 0.999
-discount = 0.7
-batch_size = 40
-buffer = 3000
+# Needs to be large to make progress
+discount_factor = 0.99
+batch_size = 100
+# More chance of IID observations with big buffer
+buffer_size = 10000
 log_freq = 10
+visualisation_freq = 50
 num_runs = 1
-reg_constant = 0.01
-grad_update_freq = 50
+# Don't set this too high, or else outliers skew the success
+max_episode_length = 500
 
 # Network Parameters
-n_hidden_1 = 30 # 1st layer number of nodes
-n_hidden_2 = 30 # 2nd layer number of nodes
+# Needs high enough capacity, but small enough to permit efficient fitting
+n_hidden_1 = 50 # 1st layer number of nodes
+n_hidden_2 = 50 # 2nd layer number of nodes
 n_input = 4 # input layer (state)
 n_output = 2 # output layer (q values for actions)
 
+# Create computation graph
+
 model = perceptron.MultilayerPerceptron(n_input, n_hidden_1, n_hidden_2, n_output)
-inputs = [tf.placeholder(shape=[1, n_input], dtype=tf.float32)]
+inputs = tf.placeholder(shape=[1, n_input], dtype=tf.float32)
 
-# Construct output
-current_output = model.evaluate(inputs[0])
+current_output, reg = model.evaluate(inputs)
+target_output = tf.placeholder(shape=[1, n_output],dtype=tf.float32)
 
-# current_output = lstm_network.lstm_network(inputs, weights, biases)
-target_output = tf.placeholder(shape=[1,n_output],dtype=tf.float32)
+loss = tf.reduce_sum(tf.square(target_output - current_output)) + reg
+trainer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
-# Define loss (with regularization) and trainer
-tvars = tf.trainable_variables()
-reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-loss = tf.reduce_sum(tf.square(target_output - current_output)) + reg_constant * sum(reg_losses)
-trainer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-# frozen network for stability
-W1Grad = tf.placeholder(tf.float32,name="batch_grad1")
-W2Grad = tf.placeholder(tf.float32,name="batch_grad2")
-WoutGrad = tf.placeholder(tf.float32,name="batch_grad3")
-batchGrad = [W1Grad,W2Grad,WoutGrad]
-# ignore hidden weights which it somehow knows we defined
-newGrads = tf.gradients(loss, tvars)[0:3]
-updateGrads = trainer.apply_gradients(zip(batchGrad, tvars))
-# updateModel = trainer.minimize(loss)
+minimize = trainer.minimize(loss)
 
 init = tf.initialize_all_variables()
+
 rewardToPlot = []
-lossToPlot = []
 
 for alpha in range(num_runs):
-    print("ALPHA = " + str(alpha))
-    # Create lists to contain total rewards and steps per episode
-    jList = []
+    print("Run # = {}".format(str(alpha)))
+    # Create lists to contain total rewards per episode
     rList = []
-    sList = []
     rAggList = []
-    lossAggList = []
-    lossList = []
-    stepUntilDoneList = []
+
     # Stores tuples of (S, A, R, S')
-    # For prioritized experience replay
+    # For experience replay
     replay = []
-    priority = []
     h = 0
     eps = initial_eps
-    grad_count = 0
+
     with tf.Session() as sess:
         sess.run(init)
-        gradBuffer = sess.run(tvars)
-        for ix, grad in enumerate(gradBuffer):
-            gradBuffer[ix] = grad * 0
         for i in range(num_episodes):
-            # learning_rate = learning_rate - learning_rate / (num_episodes)
+            # anneal epsilon
+            eps = max(eps - (initial_eps / 100 ), 0.05)
             # Reset environment and get first new observation
-            s = copy.copy(env.reset())
+            s = env.reset()
             rTotal = 0
             lossTotal = 0
-            d = False
             j = 0
             # The q-network
-            while j < 1000:
-                # env.render()
+            while j < max_episode_length:
+                if i % visualisation_freq == 0:
+                    env.render()
                 j+=1
-                # Choose an action greedily (with e chance of random action) from the Q-network
-                q = sess.run([current_output], feed_dict={inputs[0]: [s]})[0]
+                if j == max_episode_length:
+                    print('hit the buffers')
+                # Choose an action greedily from the Q-network
+                q = sess.run(current_output, feed_dict={inputs: [s]})[0]
                 a = np.argmax(q)
                 if np.random.rand(1) < eps:
-                    # a = env.sample_action()
                     a = env.action_space.sample()
                 # Get new state and reward from environment
-                s1,r,d,_ = env.step(a)
+                sPrime, r, d, _ = env.step(a)
                 if d:
-                    # Penalise failure (should we do this?)
-                    if r < 200:
-                        r -= 50
-                if len(replay) < buffer:  # if buffer not filled, add to it
-                    replay.append((s, a, r, s1))
-                    priority.append(0.001)
-                    if d:
-                        rTotal += r
-                        break
-                # buffer is full - overwrite values
+                    # Nowhere to go in terminal state
+                    sPrime = None
+                # If buffer not filled, add to it
+                if len(replay) < buffer_size:
+                    replay.append((s, a, r, sPrime))
+                # Buffer is full - overwrite values
                 else:
-                    if (h < (buffer - 1)):
+                    if h < (buffer_size - 1):
                         h += 1
                     else:
                         h = 0
-                    replay[h] = (s, a, r, s1)
-                    priority[h] = 0.001 # np.max(priority)
-                s = copy.copy(s1)
-                rTotal += r
-                # randomly sample our experience replay memory
-                # minibatch_indices = np.random.choice(range(len(replay)), batch_size,
-                                                     # p=[np.sqrt(a)/sum([np.sqrt(b) for b in priority]) for a in priority])
-                if not (batch_size < len(replay)):
+                    replay[h] = (s, a, r, sPrime)
+                # Prepare for next iteration
+                if d:
                     break
+                s = sPrime
+                rTotal += r
+            # Update only once per episode for stability
+            if batch_size < len(replay):
+                # Randomly sample our experience replay memory
                 minibatch_indices = np.random.choice(range(len(replay)), batch_size)
                 for index in minibatch_indices:
-                    temp_loss = 0
-                    memory = replay[index]
-                    s_mem, a_mem, r_mem, s1_mem = memory
-                    target_q = sess.run(current_output, feed_dict={inputs[0]: [s_mem]})[0]
-                    # Obtain the Q' values by feeding the new state through our network
-                    q_prime = sess.run(current_output, feed_dict={inputs[0]: [s1_mem]})[0]
-                    # Obtain max Q' and set our target value for chosen action
-                    max_q_prime = np.max(q_prime)
+                    s_mem, a_mem, r_mem, sPrime_mem = replay[index]
+                    target_q = sess.run(current_output, feed_dict={inputs: [s_mem]})[0]
+                    # Expected return in the terminal state is known to be 0
+                    max_q_prime = 0
+                    if sPrime_mem is not None:
+                        # Obtain the Q' values by feeding the new state through our network
+                        q_prime = sess.run(current_output, feed_dict={inputs: [sPrime_mem]})[0]
+                        # Obtain max Q' and set our target value for chosen action
+                        max_q_prime = np.max(q_prime)
                     # Q-learning
-                    target_q[a_mem] = np.clip(r_mem + discount * max_q_prime,-200,300)
-                    # print(target_q[a_mem])
-                    # print(target_q)
-                    # SARSA
-                    # target_q[a_mem] = np.clip(r_mem + discount * q_prime[a_mem],-1,1)
-                    # Train our network using target and predicted Q values
-                    # why do we have to do these sequentially?
-                    tGrad = sess.run(newGrads, feed_dict={inputs[0]: [s_mem], target_output: [target_q]})
-                    loss_val = sess.run(loss, feed_dict={inputs[0]: [s_mem], target_output: [target_q]})
-                    # priority[index] = abs(loss_val)
-                    temp_loss += loss_val
-                    for ix, grad in enumerate(tGrad):
-                        # accumulate the gradients
-                        gradBuffer[ix] += grad
-                    grad_count += 1
-                    if grad_count % grad_update_freq == 0:
-                        sess.run(updateGrads, feed_dict={W1Grad: gradBuffer[0], W2Grad: gradBuffer[1], WoutGrad: gradBuffer[2]})
-                        for ix, grad in enumerate(gradBuffer):
-                            gradBuffer[ix] = grad * 0
-                lossTotal += temp_loss / batch_size
-                if d:
-                    # Reduce chance of random action as we train the model
-                    # eps -= initial_eps / (num_episodes)
-                    eps = eps * eps_decay
-                    eps = max(eps, 1e-5)
-                    # get loss per iteration, a better measure
-                    lossTotal = lossTotal / j
-                    # replay.append((s, a, r, s1))
-                    # priority.append(0.001)
-                    stepUntilDoneList.append(j)
-                    break
-            # sList.append(s)
-            # jList.append(j)
+                    target_q[a_mem] = r_mem + discount_factor * max_q_prime
+                    sess.run(minimize, feed_dict={inputs: [s_mem], target_output: [target_q]})
             rList.append(rTotal)
-            lossList.append(lossTotal)
             if i % log_freq == 0:
                 rAggList.append(np.mean(rList[-log_freq:]))
-                lossAggList.append(np.mean(lossList[-log_freq:]))
                 print("Average reward after " + str(i) + " episodes = " + str(np.mean(rList[-log_freq:])))
-                print("Average loss after " + str(i) + " episodes = " + str(np.mean(lossList[-log_freq:])))
-                # print("Average basket after " + str(i) + " episodes = " + str(np.mean(sList[-log_freq:],axis=0)))
-                # print("Average steps until done after " + str(i) + " episodes = " + str(np.mean(stepUntilDoneList[-log_freq:])))
-                # print("Epsilon after " + str(i) + " episodes = " + str(eps))
-        # Evaluation
-        print('Evaluating model...')
-        s = copy.copy(env.reset())
-        j = 0
-        while j < 10000:
-            # env.render()
-            j += 1
-            q = sess.run([current_output], feed_dict={inputs[0]: [s]})[0]
-            # print(q)
-            a = np.argmax(q)
-            s1, r, d, _ = env.step(a)
-            # print(str(s) +',' + str(a) + '->' + str(s1))
-            if d:
-                print('failed after ' + str(j) + ' timesteps')
+            if i > 100 and np.mean(rList[-100:]) > 200:
+                print("Mission complete after {} episodes! The average over the last 100 was {}. Visualising...".\
+                    format(i, np.mean(rList[-100:])))
+                s = env.reset()
+                j = 0
+                while j < max_episode_length:
+                    env.render()
+                    j += 1
+                    if j == max_episode_length:
+                        print("Pole did not fall in first {} steps".format(max_episode_length))
+                    # Choose an action greedily from the Q-network
+                    q = sess.run(current_output, feed_dict={inputs: [s]})[0]
+                    a = np.argmax(q)
+                    # Get new state and reward from environment
+                    sPrime, r, d, _ = env.step(a)
+                    if d:
+                        print("Pole fell on step {}".format(j))
+                        break
+                    s = sPrime
                 break
-            s = copy.copy(s1)
     rewardToPlot.append(rAggList)
-    lossToPlot.append(lossAggList)
 
 plt.plot(np.mean(rewardToPlot,axis=0))
 plt.savefig( 'reward.png' )
 
 plt.clf()
-
-plt.plot(np.mean(lossToPlot,axis=0))
-plt.savefig( 'loss.png' )
--- perceptron.py
+++ perceptron.py
@@ -6,25 +6,27 @@ class MultilayerPerceptron:
     def __init__(self, n_input, n_hidden_1, n_hidden_2, n_output):
         # Store layers weight & bias
         self.weights = {
-            'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1], stddev=0.1)), # could use xavier_optimiser (what's the advantage?)
-            'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], stddev=0.1)),
-            'out': tf.Variable(tf.random_normal([n_hidden_2, n_output], stddev=0.1))
+            'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1], stddev=0.01)),
+            'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], stddev=0.01)),
+            'out': tf.Variable(tf.random_normal([n_hidden_2, n_output], stddev=0.01))
         }
-        """
         self.biases = {
-            'b1': tf.Variable(tf.random_uniform([n_hidden_1], minval=-0.1, maxval=0.1)),
-            'b2': tf.Variable(tf.random_uniform([n_hidden_2], minval=-0.1, maxval=0.1)),
-            'out': tf.Variable(tf.random_uniform([n_output], minval=-0.1, maxval=0.1))
+            'b1': tf.Variable(tf.zeros([n_hidden_1])),
+            'b2': tf.Variable(tf.zeros([n_hidden_2])),
+            'out': tf.Variable(tf.zeros([n_output]))
         }
-        """
+        self.reg = 0.001
 
     def evaluate(self, x):
-        # Hidden layer with sigmoid activation
-        layer_1 = tf.add(tf.matmul(x, self.weights['h1']), 0) #self.biases['b1']
+        # Hidden layer with tanh activation
+        layer_1 = tf.add(tf.matmul(x, self.weights['h1']), self.biases['b1'])
         layer_1 = tf.nn.tanh(layer_1)
-        # Hidden layer with sigmoid activation
-        layer_2 = tf.add(tf.matmul(layer_1, self.weights['h2']), 0) #self.biases['b2']
+        # Hidden layer with tanh activation
+        layer_2 = tf.add(tf.matmul(layer_1, self.weights['h2']), self.biases['b2'])
         layer_2 = tf.nn.tanh(layer_2)
         # Output layer with linear activation
-        out_layer = tf.matmul(layer_2, self.weights['out'])#  + self.biases['out']
-        return out_layer
+        out_layer = tf.matmul(layer_2, self.weights['out']) + self.biases['out']
+        reg = self.reg * (tf.reduce_sum(tf.square(self.weights['h1']))
+                          + tf.reduce_sum(tf.square(self.weights['h2']))
+                          + tf.reduce_sum(tf.square(self.weights['out'])))
+        return out_layer, reg
